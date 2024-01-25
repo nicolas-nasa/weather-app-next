cb892c35e9d6b1315ad228aa4e4c656fd1d611db
Nicolas R Almeida <nicolas_ariel_almeida@outlook.com>
2024-01-24 23:49:02 -0300

    chore: add use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import SearchInput from '@/components/molecules/search'
 import Header from '@/components/organisms/header'
 import WeatherCard, { IToday } from '@/components/organisms/weatherCard'
@@ -16,8 +18,6 @@ export default function Home() {
     latitude: number
     longitude: number
   }>()
-
-  const setChangeBetweenTemperature = () => {}
 
   const setWhether = async (pos: { latitude: number; longitude: number }) => {
     const today = await fetch(
